18f44977bc9cc1d93b99dc13f4f700f81de30f86
Rathan Kumar <rathan.kumar492@gmail.com>
2021-03-25 04:53:29 +0530

    allowed host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greatkart-course-env.eba-pepcery4.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
