b60ac565d89bd382d49a812c563a1ed5ec5f9c1e
Rathan Kumar <rathan.kumar492@gmail.com>
2021-03-25 05:36:04 +0530

    fixed reviews issue

--- greatkart/views.py
+++ greatkart/views.py
@@ -5,6 +5,7 @@ def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
 
     # Get the reviews
+    reviews = None
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
 
